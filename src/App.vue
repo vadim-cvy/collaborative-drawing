76c5a37810818209599f4a25c7bee00606db117d
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-06-17 13:19:57 +0300

    Create draft html architecture

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>
+      Collaborative Drawing App by <a href="https://github.com/vadim-cvy" target="_blank">Vadim Cherepenichev</a>
+    </h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- todo -->
+    App description goes here
   </main>
+
+  <aside>
+    <div>
+      <ul>
+        <li>tool</li>
+        <li>tool</li>
+      </ul>
+
+      <ul>
+        <li>selected tool setting</li>
+        <li>selected tool setting</li>
+      </ul>
+
+      <ul>
+        <li>selected tool color 1</li>
+        <li>selected tool color 2</li>
+      </ul>
+    </div>
+
+    <div>
+      board
+    </div>
+  </aside>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
